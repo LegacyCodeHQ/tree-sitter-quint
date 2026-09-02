06eb7773af62aaf4fe87bfe64bc2f8289ebe26b9
Ragunath Jawahar <ragunath@legacycode.com>
2026-09-02 16:32:17 +0530

    test: add snapshot litmus test

--- test/litmus.test.js
+++ test/litmus.test.js
@@ -3,3 +3,7 @@ import { expect, test } from "bun:test";
 test("regular assertions are enforced", () => {
   expect(1 + 1).toBe(2);
 });
+
+test("snapshot assertions compare approved output", () => {
+  expect({ language: "quint", parser: "tree-sitter" }).toMatchSnapshot();
+});
