ab3f35a2a201a2cc5da068b6c6df46b1c843b3b3
Ragunath Jawahar <ragunath@legacycode.com>
2026-09-02 19:28:25 +0530

    feat: parse annotated run operators

--- grammar.js
+++ grammar.js
@@ -172,11 +172,21 @@ export default grammar({
         seq(
           field("qualifier", "run"),
           field("name", $.identifier),
-          optional(seq(
-            "(",
-            field("parameter", $.parameter),
-            repeat(seq(",", field("parameter", $.parameter))),
-            ")",
+          optional(choice(
+            seq(
+              "(",
+              field("parameter", $.annotated_parameter),
+              repeat(seq(",", field("parameter", $.annotated_parameter))),
+              ")",
+              ":",
+              field("return_type", $._type),
+            ),
+            seq(
+              "(",
+              field("parameter", $.parameter),
+              repeat(seq(",", field("parameter", $.parameter))),
+              ")",
+            ),
           )),
         ),
       ),
