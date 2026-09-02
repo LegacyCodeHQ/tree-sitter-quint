96d55cc847b5a46efa122696e4797d4af917daf0
Ragunath Jawahar <ragunath@legacycode.com>
2026-09-02 19:03:26 +0530

    feat: parse annotated actions

--- grammar.js
+++ grammar.js
@@ -100,11 +100,21 @@ export default grammar({
         seq(
           field("qualifier", "action"),
           field("name", $.identifier),
-          optional(seq(
-            "(",
-            field("parameter", $.parameter),
-            repeat(seq(",", field("parameter", $.parameter))),
-            ")",
+          optional(choice(
+            seq(
+              "(",
+              field("parameter", $.annotated_parameter),
+              repeat(seq(",", field("parameter", $.annotated_parameter))),
+              ")",
+              ":",
+              field("return_type", $._type),
+            ),
+            seq(
+              "(",
+              field("parameter", $.parameter),
+              repeat(seq(",", field("parameter", $.parameter))),
+              ")",
+            ),
           )),
         ),
       ),
